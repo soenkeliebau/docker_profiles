ff36bfc2db87e8161e765dfed5bbac9cbf0a0bab
Sönke Liebau <soenke.liebau@opencore.com>
2016-05-25 13:15:39 +0200

    Added logging lines.

--- generate_profiles.sh
+++ generate_profiles.sh
@@ -6,13 +6,16 @@ mkdir -p /documents/build/pdf
 cp -r /documents/resources /documents/build/
 
 for template in /documents/templates/*.j2; do
+    template=$(basename "$template")
+    echo "Beginning processing for template $template.."
     for profile in /documents/input/*.json; do
         filename=$(basename "$profile")
         filename="${filename%.*}"
         adocfile="/documents/build/${filename}.adoc"
         pdffile="/documents/build/pdf/${filename}.pdf"
-        template=$(basename "$template")
+        echo "Generating intermediate file from $filename into $adocfile"
         python /usr/local/bin/main.py -t $template -i $profile -o $adocfile
+        echo "Writing $pdffile"
         asciidoctor -r asciidoctor-pdf -b pdf $adocfile -o $pdffile
     done
 done
